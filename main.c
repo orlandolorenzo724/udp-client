2bde94c93c3e496284584b87d0fa6b5a3ed0f333
Lorenzo Orlando <lorenzo.orlando724@gmail.com>
2021-04-01 14:37:40 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
     if(argc < 3)  {
         puts("Format: ./main <server name> <server port>");
-        exit(EXIT_SUCCESS);
+        exit(EXIT_FAILURE);
     }
 
     time_t time = run(argv[1], atoi(argv[2]));
